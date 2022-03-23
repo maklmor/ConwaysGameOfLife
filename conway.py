4830f2966349c40cd89d60079470f872ed49f02d
maklmor <mikiinblack@gmail.com>
2022-03-23 23:29:09 +0100

    implemented Animator class and refactored conway code into independent class Conway

--- conway.py
+++ conway.py
@@ -1,32 +1,51 @@
-def next_gen(cells):
-    next_state = []
-    for y in range(len(cells)):
-        next_row = []
-        for x in range(len(cells[y])):
-            # determine the next state of this cell
-            neighbours = survey_peers(cells, x, y)
-            if neighbours > 3 or neighbours < 2:
-                state = 0
-            elif neighbours == 3:
-                state = 1
-            else:
-                # preserve previous state
-                state = cells[y][x]
+from typing import List
 
-            next_row.append(state)
-        next_state.append(next_row)
-    return next_state
 
-def survey_peers(arr: list, x: int, y: int) -> int:
-    """returns the number of live neighbouring cells"""
-    count = 0
-    # pre-made template to survey all possible neighbours
-    neighbours = ((y-1, x-1), (y-1, x), (y-1, x+1), # upper row
-                  (y, x-1),             (y, x+1),   # adjacent cells
-                  (y+1, x-1), (y+1, x), (y+1, x+1)) # cells below
+class Conway:
+  def __init__(self, state):
+    self.state = state
 
-    # loops through all neighbouring cells and checks those that are inside our NxM plane
-    for r, c in neighbours:
-        if r >= 0 and r < len(arr) and c >= 0 and c < len(arr[0]):
-            count += arr[r][c]
-    return count
+
+  def get_state(self) -> List[List[int]]:
+    state = self.state
+    self.change_state()
+    return state
+
+
+  def change_state(self) -> None:
+    self.state = self.next_gen(self.state)
+
+
+  def next_gen(self, cells):
+      next_state = []
+      for y in range(len(cells)):
+          next_row = []
+          for x in range(len(cells[y])):
+              # determine the next state of this cell
+              neighbours = self.survey_peers(cells, x, y)
+              if neighbours > 3 or neighbours < 2:
+                  state = 0
+              elif neighbours == 3:
+                  state = 1
+              else:
+                  # preserve previous state
+                  state = cells[y][x]
+
+              next_row.append(state)
+          next_state.append(next_row)
+      return next_state
+
+
+  def survey_peers(self, arr: list, x: int, y: int) -> int:
+      """returns the number of live neighbouring cells"""
+      count = 0
+      # pre-made template to survey all possible neighbours
+      neighbours = ((y-1, x-1), (y-1, x), (y-1, x+1), # upper row
+                    (y, x-1),             (y, x+1),   # adjacent cells
+                    (y+1, x-1), (y+1, x), (y+1, x+1)) # cells below
+
+      # loops through all neighbouring cells and checks those that are inside our NxM plane
+      for r, c in neighbours:
+          if r >= 0 and r < len(arr) and c >= 0 and c < len(arr[0]):
+              count += arr[r][c]
+      return count
---
+++ graphics.py
@@ -0,0 +1,40 @@
+from conway import Conway
+from tkinter import Canvas, Tk
+
+SCALE = 100
+FRAME_LENGTH = 500 # in ms
+starting_state = [
+  [1,0,1,0,1],
+  [0,1,0,1,0],
+  [1,0,1,0,1],
+  [0,1,0,1,0],
+  [1,0,1,0,1]
+  ]
+
+class Animator:
+  """Dummy class in order to preserve independence of Conway class from graphics"""
+  def __init__(self, root, data_source):
+      """data_source should be a Object that posseses get_state() method"""
+      self.root = root # root window
+      self.data_source = data_source
+
+  def draw(self):
+    state = self.data_source.get_state()
+    for y in range(len(state)):
+      for x in range(len(state[y])):
+        color = "black" if state[y][x] else "white"
+        canvas.create_rectangle(x * SCALE, y * SCALE, (x+1) * SCALE, (y+1) * SCALE, fill=color)
+    self.root.after(FRAME_LENGTH, self.draw)
+
+
+if __name__=="__main__":
+  root = Tk()
+  canvas = Canvas(root, height=500, width=500)
+  canvas.pack()
+
+  # Main animation loop
+  conway = Conway(starting_state)
+  animator = Animator(root, conway)
+  root.after(FRAME_LENGTH, animator.draw)
+
+  root.mainloop()
